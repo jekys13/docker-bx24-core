5ea7a65b0ed8bed8bb5c72553e0f80bc5c8a17e0
Evgeny Prokhorov <prohorov-evgen@ya.ru>
2019-07-10 15:04:20 +0300

    some changes for access permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,10 +23,10 @@ else
         echo 'Applying MySQL dump'
         mysql --user=$MYSQL_USER --password=$MYSQL_PASSWORD --host=db --database=$MYSQL_DATABASE < bitrix/dump.sql
 
-        chmod 755 bitrix/
-        chown -R $HOST_USER:$HOST_USER bitrix/
-        
         rm -rf $PROJECT_NAME.tar.gz
         rm -rf bitrix/dump.sql
+
+        chmod 755 bitrix/
+        chown -R $HOST_USER bitrix/
     fi
 fi
